bb6d1cab3eb9b3083707b18c42141831af1b15e1
gondaimgano <gondaimgano@gmail.com>
2021-09-28 13:24:21 +0200

    refactored Index sealed class

--- app/src/main/java/gondai/tutorial/mvvmcomposebptracker/MainActivity.kt
+++ app/src/main/java/gondai/tutorial/mvvmcomposebptracker/MainActivity.kt
@@ -7,18 +7,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import gondai.tutorial.mvvmcomposebptracker.screens.Index
-import gondai.tutorial.mvvmcomposebptracker.screens.history.HistoryScreen
-import gondai.tutorial.mvvmcomposebptracker.screens.history.HistoryViewModel
-import gondai.tutorial.mvvmcomposebptracker.screens.new_entry.NewEntryScreen
-import gondai.tutorial.mvvmcomposebptracker.screens.new_entry.NewEntryViewModel
 import gondai.tutorial.mvvmcomposebptracker.ui.theme.MVVMComposeBPTrackerTheme
-
 
 
 @AndroidEntryPoint
@@ -49,8 +43,8 @@ class MainActivity : ComponentActivity() {
 fun BPTrackerApp(){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Index.History.route) {
-        composable(Index.History.route) { HistoryScreen(navController = navController) }
-        composable(Index.NewEntry.route) { NewEntryScreen(navController=navController) }
+        composable(Index.History.route) { Index.History.screen(navController) }
+        composable(Index.NewEntry.route) { Index.NewEntry.screen(navController) }
 
     }
 }
--- app/src/main/java/gondai/tutorial/mvvmcomposebptracker/screens/Index.kt
+++ app/src/main/java/gondai/tutorial/mvvmcomposebptracker/screens/Index.kt
@@ -1,9 +1,18 @@
 package gondai.tutorial.mvvmcomposebptracker.screens
 
 import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 import gondai.tutorial.mvvmcomposebptracker.R
+import gondai.tutorial.mvvmcomposebptracker.screens.history.HistoryScreen
+import gondai.tutorial.mvvmcomposebptracker.screens.new_entry.NewEntryScreen
 
-sealed class Index(val route: String, @StringRes val resourceId: Int,val label:String) {
-    object History : Index("history", R.string.history,"History")
-    object NewEntry : Index("newentry", R.string.new_entry,"Add Reading")
+sealed class Index(val route: String, @StringRes val resourceId: Int,val label:String,val screen:@Composable (NavController)->Unit) {
+    object History : Index("history", R.string.history,"History",{
+        HistoryScreen(navController = it)
+
+    })
+    object NewEntry : Index("newentry", R.string.new_entry,"Add Reading",{
+        NewEntryScreen(navController = it)
+    })
 }
